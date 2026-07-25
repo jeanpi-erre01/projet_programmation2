746dedcda2b35bd56a2bf149688b9518f20f9eb3
Maimouna ndiaye <Maimouna.ndiaye@monboreal.ca>
2026-07-25 00:41:51 -0400

    partie 7

--- goran.py
+++ goran.py
@@ -1,321 +1,321 @@
-# """partie 4 """
+"""partie 4 """
 
-# class MembreStandart(Membre):
-#     def _init_(self,casier,numero,nom,succursale,duree,prix_mens,actif):
-#         super._init_(self,numero,nom,succursale,duree,prix_mens,actif)
-#         self.casier=casier
-# def afficher(self):
-#         super().afficher()
-#         print(f"Casier : {'Oui' if self.casier else 'Non'}")
+class MembreStandart(Membre):
+    def _init_(self,casier,numero,nom,succursale,duree,prix_mens,actif):
+        super._init_(self,numero,nom,succursale,duree,prix_mens,actif)
+        self.casier=casier
+def afficher(self):
+        super().afficher()
+        print(f"Casier : {'Oui' if self.casier else 'Non'}")
 
-# class Membrepremium(Membre):
-#     def _init_(self,coach_perso,numero,nom,succursale,duree,prix_mens,actif):
-#         super._init_(numero,nom,succursale,duree,prix_mens,actif)
-#         self.coach_perso=coach_perso
+class Membrepremium(Membre):
+    def _init_(self,coach_perso,numero,nom,succursale,duree,prix_mens,actif):
+        super._init_(numero,nom,succursale,duree,prix_mens,actif)
+        self.coach_perso=coach_perso
 
-#     def afficher(self):
-#         super().afficher()
-#         print(f"Coach personnel : {'Oui' if self.coach_perso else 'Non'}")
-# membre1 = MembreStandart(True, 1, "ruth", "Montreal", 12, 50, True)
-# membre2 = MembreStandart(False, 2, "jean", "Toronto", 6, 40, True)
+    def afficher(self):
+        super().afficher()
+        print(f"Coach personnel : {'Oui' if self.coach_perso else 'Non'}")
+membre1 = MembreStandart(True, 1, "ruth", "Montreal", 12, 50, True)
+membre2 = MembreStandart(False, 2, "jean", "Toronto", 6, 40, True)
 
 
-# membre3 = Membrepremium(True, 3, "ahou", "Montreal", 12, 100, True)
-# membre4 = Membrepremium(False, 4, "idris", "Toronto", 6, 90, False)
+membre3 = Membrepremium(True, 3, "ahou", "Montreal", 12, 100, True)
+membre4 = Membrepremium(False, 4, "idris", "Toronto", 6, 90, False)
 
 
-# membres = [membre1, membre2, membre3, membre4]
+membres = [membre1, membre2, membre3, membre4]
 
 
-# for membre in membres:
-#     membre.afficher()
+for membre in membres:
+    membre.afficher()
 
 
 
 
 
-# """ partie 10 """
+""" partie 10 """
 
-# import random
-# import time
+import random
+import time
 
 
 
-# def hacher_membre(nom, succursale):
-#     combinaison = nom + succursale
-#     return hash(combinaison)
+def hacher_membre(nom, succursale):
+    combinaison = nom + succursale
+    return hash(combinaison)
 
 
 
 
-# def detecter_doublons_hash(membres):
+def detecter_doublons_hash(membres):
 
-#     membres_hash = set()
-#     doublons = 0
+    membres_hash = set()
+    doublons = 0
 
-#     for membre in membres:
+    for membre in membres:
 
-#         cle = hacher_membre(
-#             membre["nom"],
-#             membre["succursale"]
-#         )
+        cle = hacher_membre(
+            membre["nom"],
+            membre["succursale"]
+        )
 
-#         if cle in membres_hash:
-#             doublons += 1
-#         else:
-#             membres_hash.add(cle)
+        if cle in membres_hash:
+            doublons += 1
+        else:
+            membres_hash.add(cle)
 
-#     return doublons
+    return doublons
 
 
 
-# def detecter_doublons_lineaire(membres):
+def detecter_doublons_lineaire(membres):
 
-#     membres_ajoutes = []
-#     doublons = 0
+    membres_ajoutes = []
+    doublons = 0
 
-#     for membre in membres:
+    for membre in membres:
 
-#         est_doublon = False
+        est_doublon = False
 
-#         for ancien_membre in membres_ajoutes:
+        for ancien_membre in membres_ajoutes:
 
-#             if (membre["nom"] == ancien_membre["nom"] and
-#                     membre["succursale"] == ancien_membre["succursale"]):
+            if (membre["nom"] == ancien_membre["nom"] and
+                    membre["succursale"] == ancien_membre["succursale"]):
 
-#                 est_doublon = True
-#                 break
+                est_doublon = True
+                break
 
-#         if est_doublon:
-#             doublons += 1
-#         else:
-#             membres_ajoutes.append(membre)
+        if est_doublon:
+            doublons += 1
+        else:
+            membres_ajoutes.append(membre)
 
-#     return doublons
+    return doublons
 
 
 
 
-# noms = [
-#     "Alice",
-#     "Bob",
-#     "Charlie",
-#     "David",
-#     "Emma",
-#     "Frank",
-#     "George",
-#     "Hugo",
-#     "Julie",
-#     "Kevin"
-# ]
+noms = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Emma",
+    "Frank",
+    "George",
+    "Hugo",
+    "Julie",
+    "Kevin"
+]
 
 
 
-# succursales = [
-#     "Montreal",
-#     "Toronto",
-#     "Ottawa",
-#     "Quebec"
-# ]
+succursales = [
+    "Montreal",
+    "Toronto",
+    "Ottawa",
+    "Quebec"
+]
 
 
 
-# membres = []
+membres = []
 
-# for i in range(1000):
+for i in range(1000):
 
-#     membre = {
-#         "nom": random.choice(noms),
-#         "succursale": random.choice(succursales)
-#     }
+    membre = {
+        "nom": random.choice(noms),
+        "succursale": random.choice(succursales)
+    }
 
-#     membres.append(membre)
+    membres.append(membre)
 
 
 
 
-# debut_hash = time.perf_counter()
+debut_hash = time.perf_counter()
 
-# doublons_hash = detecter_doublons_hash(membres)
+doublons_hash = detecter_doublons_hash(membres)
 
-# fin_hash = time.perf_counter()
+fin_hash = time.perf_counter()
 
-# temps_hash = fin_hash - debut_hash
+temps_hash = fin_hash - debut_hash
 
 
 
 
-# debut_lineaire = time.perf_counter()
+debut_lineaire = time.perf_counter()
 
-# doublons_lineaire = detecter_doublons_lineaire(membres)
+doublons_lineaire = detecter_doublons_lineaire(membres)
 
-# fin_lineaire = time.perf_counter()
+fin_lineaire = time.perf_counter()
 
-# temps_lineaire = fin_lineaire - debut_lineaire
+temps_lineaire = fin_lineaire - debut_lineaire
 
 
 
 
 
 
-# print("Nombre de membres générés :", len(membres))
+print("Nombre de membres générés :", len(membres))
 
-# print("Nombre de doublons avec le hash :", doublons_hash)
+print("Nombre de doublons avec le hash :", doublons_hash)
 
-# print("Nombre de doublons avec la recherche linéaire :", doublons_lineaire)
+print("Nombre de doublons avec la recherche linéaire :", doublons_lineaire)
 
-# print()
+print()
 
-# print(
-#     f"Temps avec le set de hachage : "
-#     f"{temps_hash:.8f} secondes"
-# )
+print(
+    f"Temps avec le set de hachage : "
+    f"{temps_hash:.8f} secondes"
+)
 
-# print(
-#     f"Temps avec la recherche linéaire : "
-#     f"{temps_lineaire:.8f} secondes"
-# )
+print(
+    f"Temps avec la recherche linéaire : "
+    f"{temps_lineaire:.8f} secondes"
+)
 
 
 
-# """partie 11"""
+"""partie 11"""
 
 
-# class Membre:
+class Membre:
 
-#     def __init__(self, numero, nom, succursale, duree, prix_mens, actif):
-#         self.numero = numero
-#         self.nom = nom
-#         self.succursale = succursale
-#         self.duree = duree
-#         self.prix_mens = prix_mens
-#         self.actif = actif
+    def __init__(self, numero, nom, succursale, duree, prix_mens, actif):
+        self.numero = numero
+        self.nom = nom
+        self.succursale = succursale
+        self.duree = duree
+        self.prix_mens = prix_mens
+        self.actif = actif
 
-#     def afficher(self):
-#         print("Numéro :", self.numero)
-#         print("Nom :", self.nom)
-#         print("Succursale :", self.succursale)
-#         print("Durée :", self.duree)
-#         print("Prix mensuel :", self.prix_mens)
-#         print("Actif :", self.actif)
+    def afficher(self):
+        print("Numéro :", self.numero)
+        print("Nom :", self.nom)
+        print("Succursale :", self.succursale)
+        print("Durée :", self.duree)
+        print("Prix mensuel :", self.prix_mens)
+        print("Actif :", self.actif)
 
 
 
 
-# def construire_index(membres):
+def construire_index(membres):
 
-#     index_membres = {}
+    index_membres = {}
 
-#     for membre in membres:
-#         index_membres[membre.numero] = membre
+    for membre in membres:
+        index_membres[membre.numero] = membre
 
-#     return index_membres
+    return index_membres
 
 
 
 
-# def rechercher_par_numero(index_membres, numero):
+def rechercher_par_numero(index_membres, numero):
 
-#     if numero in index_membres:
-#         return index_membres[numero]
+    if numero in index_membres:
+        return index_membres[numero]
 
-#     return "Aucun membre ne possède le numéro " + str(numero)
+    return "Aucun membre ne possède le numéro " + str(numero)
 
 
 
 
-# def rechercher_lineaire(membres, numero):
+def rechercher_lineaire(membres, numero):
 
-#     for membre in membres:
+    for membre in membres:
 
-#         if membre.numero == numero:
-#             return membre
+        if membre.numero == numero:
+            return membre
 
-#     return None
+    return None
 
 
 
-# membres = []
+membres = []
 
-# for i in range(10000):
+for i in range(10000):
 
-#     membre = Membre(
-#         i,
-#         "Membre" + str(i),
-#         "Montreal",
-#         12,
-#         50,
-#         True
-#     )
+    membre = Membre(
+        i,
+        "Membre" + str(i),
+        "Montreal",
+        12,
+        50,
+        True
+    )
 
-#     membres.append(membre)
+    membres.append(membre)
 
 
 
-# index_membres = construire_index(membres)
+index_membres = construire_index(membres)
 
 
 
-# numero_recherche = 9999
+numero_recherche = 9999
 
 
 
 
-# debut_lineaire = time.perf_counter()
+debut_lineaire = time.perf_counter()
 
-# membre_lineaire = rechercher_lineaire(
-#     membres,
-#     numero_recherche
-# )
+membre_lineaire = rechercher_lineaire(
+    membres,
+    numero_recherche
+)
 
-# fin_lineaire = time.perf_counter()
+fin_lineaire = time.perf_counter()
 
-# temps_lineaire = fin_lineaire - debut_lineaire
+temps_lineaire = fin_lineaire - debut_lineaire
 
 
 
 
-# debut_dictionnaire = time.perf_counter()
+debut_dictionnaire = time.perf_counter()
 
-# membre_dictionnaire = rechercher_par_numero(
-#     index_membres,
-#     numero_recherche
-# )
+membre_dictionnaire = rechercher_par_numero(
+    index_membres,
+    numero_recherche
+)
 
-# fin_dictionnaire = time.perf_counter()
+fin_dictionnaire = time.perf_counter()
 
-# temps_dictionnaire = fin_dictionnaire - debut_dictionnaire
+temps_dictionnaire = fin_dictionnaire - debut_dictionnaire
 
 
 
-# print("Nombre de membres :", len(membres))
+print("Nombre de membres :", len(membres))
 
-# print("Numéro recherché :", numero_recherche)
+print("Numéro recherché :", numero_recherche)
 
-# print()
+print()
 
 
 
-# if membre_lineaire is not None:
-#     print("Membre trouvé :", membre_lineaire.nom)
-# else:
-#     print("Aucun membre trouvé")
+if membre_lineaire is not None:
+    print("Membre trouvé :", membre_lineaire.nom)
+else:
+    print("Aucun membre trouvé")
 
-# print(
-#     f"Temps de recherche linéaire : "
-#     f"{temps_lineaire:.8f} secondes"
-# )
+print(
+    f"Temps de recherche linéaire : "
+    f"{temps_lineaire:.8f} secondes"
+)
 
-# print()
+print()
 
 
 
-# if isinstance(membre_dictionnaire, Membre):
-#     print("Membre trouvé :", membre_dictionnaire.nom)
-# else:
-#     print(membre_dictionnaire)
+if isinstance(membre_dictionnaire, Membre):
+    print("Membre trouvé :", membre_dictionnaire.nom)
+else:
+    print(membre_dictionnaire)
 
-# print(
-#     f"Temps de recherche avec dictionnaire : "
-#     f"{temps_dictionnaire:.8f} secondes"
-# )
+print(
+    f"Temps de recherche avec dictionnaire : "
+    f"{temps_dictionnaire:.8f} secondes"
+)
